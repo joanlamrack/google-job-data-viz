77ddaf1d74df3fb67445bb4716d5e1d873cb2989
joanlamrack <joanlamrack@gmail.com>
2018-09-03 14:32:28 +0700

    Zoomable circle paclking

--- js/script.js
+++ js/script.js
@@ -1,159 +1,122 @@
-const width = window.innerWidth,
-	height = window.innerHeight,
-	maxRadius = Math.min(width, height) / 2 - 5;
+var svg = d3.select("svg"),
+	margin = 20,
+	diameter = +svg.attr("width"),
+	g = svg
+		.append("g")
+		.attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
-const formatNumber = d3.format(",d");
-
-const x = d3
+var color = d3
 	.scaleLinear()
-	.range([0, 2 * Math.PI])
-	.clamp(true);
+	.domain([-1, 5])
+	.range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+	.interpolate(d3.interpolateHcl);
 
-const y = d3.scaleSqrt().range([maxRadius * 0.1, maxRadius]);
+var pack = d3
+	.pack()
+	.size([diameter - margin, diameter - margin])
+	.padding(2);
 
-const color = d3.scaleOrdinal(d3.schemeCategory20);
+d3.json("https://gist.githubusercontent.com/joanlamrack/318f3f0d0dda61eb97c314bbcd9fea8b/raw/c7dd652415548432103d0024b47c4ddb603c92aa/google_jobs_data", function(error, root) {
+	if (error) throw error;
 
-const partition = d3.partition();
-
-const arc = d3
-	.arc()
-	.startAngle(d => x(d.x0))
-	.endAngle(d => x(d.x1))
-	.innerRadius(d => Math.max(0, y(d.y0)))
-	.outerRadius(d => Math.max(0, y(d.y1)));
-
-const middleArcLine = d => {
-	const halfPi = Math.PI / 2;
-	const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
-	const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
-
-	const middleAngle = (angles[1] + angles[0]) / 2;
-	const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
-	if (invertDirection) {
-		angles.reverse();
-	}
-
-	const path = d3.path();
-	path.arc(0, 0, r, angles[0], angles[1], invertDirection);
-	return path.toString();
-};
-
-const textFits = d => {
-	const CHAR_SPACE = 6;
-
-	const deltaAngle = x(d.x1) - x(d.x0);
-	const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
-	const perimeter = r * deltaAngle;
-
-	return d.data.name.length * CHAR_SPACE < perimeter;
-};
-
-const svg = d3
-	.select("body")
-	.append("svg")
-	.style("width", "100vw")
-	.style("height", "100vh")
-	.attr("viewBox", `${-width / 2} ${-height / 2} ${width} ${height}`)
-	.on("click", () => focusOn()); // Reset zoom on canvas click
-
-d3.json(
-	"https://gist.githubusercontent.com/joanlamrack/318f3f0d0dda61eb97c314bbcd9fea8b/raw/c7dd652415548432103d0024b47c4ddb603c92aa/google_jobs_data",
-	(error, root) => {
-		if (error) throw error;
-
-		root = d3.hierarchy(root);
-		root.sum(d => d.size);
-
-		const slice = svg.selectAll("g.slice").data(partition(root).descendants());
-
-		slice.exit().remove();
-
-		const newSlice = slice
-			.enter()
-			.append("g")
-			.attr("class", "slice")
-			.on("click", d => {
-				d3.event.stopPropagation();
-				focusOn(d);
-			});
-
-		newSlice
-			.append("title")
-			.text(d => d.data.name + "\n" + formatNumber(d.value));
-
-		newSlice
-			.append("path")
-			.attr("class", "main-arc")
-			.style("fill", d => color((d.children ? d : d.parent).data.name))
-			.attr("d", arc);
-
-		newSlice
-			.append("path")
-			.attr("class", "hidden-arc")
-			.attr("id", (_, i) => `hiddenArc${i}`)
-			.attr("d", middleArcLine);
-
-		const text = newSlice
-			.append("text")
-			.attr("display", d => (textFits(d) ? null : "none"));
-
-		// Add white contour
-		text
-			.append("textPath")
-			.attr("startOffset", "50%")
-			.attr("xlink:href", (_, i) => `#hiddenArc${i}`)
-			.text(d => d.data.name)
-			.style("fill", "none")
-			.style("stroke", "#fff")
-			.style("stroke-width", 5)
-			.style("stroke-linejoin", "round");
-
-		text
-			.append("textPath")
-			.attr("startOffset", "50%")
-			.attr("xlink:href", (_, i) => `#hiddenArc${i}`)
-			.text(d => d.data.name);
-	}
-);
-
-function focusOn(d = { x0: 0, x1: 1, y0: 0, y1: 1 }) {
-	// Reset to top-level if no data point specified
-
-	const transition = svg
-		.transition()
-		.duration(750)
-		.tween("scale", () => {
-			const xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-				yd = d3.interpolate(y.domain(), [d.y0, 1]);
-			return t => {
-				x.domain(xd(t));
-				y.domain(yd(t));
-			};
+	root = d3
+		.hierarchy(root)
+		.sum(function(d) {
+			return d.size;
+		})
+		.sort(function(a, b) {
+			return b.value - a.value;
 		});
 
-	transition.selectAll("path.main-arc").attrTween("d", d => () => arc(d));
+	var focus = root,
+		nodes = pack(root).descendants(),
+		view;
 
-	transition
-		.selectAll("path.hidden-arc")
-		.attrTween("d", d => () => middleArcLine(d));
+	var circle = g
+		.selectAll("circle")
+		.data(nodes)
+		.enter()
+		.append("circle")
+		.attr("class", function(d) {
+			return d.parent
+				? d.children
+					? "node"
+					: "node node--leaf"
+				: "node node--root";
+		})
+		.style("fill", function(d) {
+			return d.children ? color(d.depth) : null;
+		})
+		.on("click", function(d) {
+			if (focus !== d) zoom(d), d3.event.stopPropagation();
+		});
 
-	transition
+	var text = g
 		.selectAll("text")
-		.attrTween("display", d => () => (textFits(d) ? null : "none"));
+		.data(nodes)
+		.enter()
+		.append("text")
+		.attr("class", "label")
+		.style("fill-opacity", function(d) {
+			return d.parent === root ? 1 : 0;
+		})
+		.style("display", function(d) {
+			return d.parent === root ? "inline" : "none";
+		})
+		.text(function(d) {
+			return d.data.name;
+		});
 
-	moveStackToFront(d);
+	var node = g.selectAll("circle,text");
 
-	//
+	svg.style("background", color(-1)).on("click", function() {
+		zoom(root);
+	});
 
-	function moveStackToFront(elD) {
-		svg
-			.selectAll(".slice")
-			.filter(d => d === elD)
-			.each(function(d) {
-				this.parentNode.appendChild(this);
-				if (d.parent) {
-					moveStackToFront(d.parent);
-				}
+	zoomTo([root.x, root.y, root.r * 2 + margin]);
+
+	function zoom(d) {
+		var focus0 = focus;
+		focus = d;
+
+		var transition = d3
+			.transition()
+			.duration(d3.event.altKey ? 7500 : 750)
+			.tween("zoom", function(d) {
+				var i = d3.interpolateZoom(view, [
+					focus.x,
+					focus.y,
+					focus.r * 2 + margin
+				]);
+				return function(t) {
+					zoomTo(i(t));
+				};
+			});
+
+		transition
+			.selectAll("text")
+			.filter(function(d) {
+				return d.parent === focus || this.style.display === "inline";
+			})
+			.style("fill-opacity", function(d) {
+				return d.parent === focus ? 1 : 0;
+			})
+			.on("start", function(d) {
+				if (d.parent === focus) this.style.display = "inline";
+			})
+			.on("end", function(d) {
+				if (d.parent !== focus) this.style.display = "none";
 			});
 	}
-}
+
+	function zoomTo(v) {
+		var k = diameter / v[2];
+		view = v;
+		node.attr("transform", function(d) {
+			return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")";
+		});
+		circle.attr("r", function(d) {
+			return d.r * k;
+		});
+	}
+});
